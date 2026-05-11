24551cfdad42efa5b132752b53bf50b10344aa36
genewgeorge76 <genewgeorge@gmail.com>
2026-05-11 01:01:05 -0400

    fix: remove splitVendorChunkPlugin conflict, add Chrome for prerender

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
@@ -7,7 +7,7 @@ export default defineConfig({
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [react()],
   build: {
     outDir:    'dist',
     sourcemap: false,
